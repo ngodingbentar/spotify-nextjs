b48760a7c04a9363eaba0d11776f6fb9dbe2660d
ngodingbentar <dewaqintoro@gmail.com>
2022-08-11 01:09:01 +0700

    initial

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar"
+const Home = () => {
+  return (
+    <div>
+      <main>
+        <Sidebar />
+      </main>
+    </div>
+  )
+}
+
+export default Home
